6f25746774c78de89ffbf99b26bdb6b23878c36a
Nurizki <nutrisi1udin@gmail.com>
2022-10-02 20:12:41 +0700

    tugas 14 js
    tugas niomic

---
+++ tugas14.js
@@ -0,0 +1,11 @@
+function math14(){
+    var x = 37;
+    var hasil = x/23;
+
+    return hasil
+}
+
+console.log(math14());
+console.log(Math.round(math14()));
+console.log(Math.ceil(math14()));
+console.log(Math.floor(math14()));
